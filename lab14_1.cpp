c61ed14d33e784ccdaea01c2b09bae6fe0d46e95
PackYokohama <59277976+PackYokohama@users.noreply.github.com>
2020-02-25 17:21:21 +0700

    Update lab14_1.cpp

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,10 +1,56 @@
-include <iostream>
+#include <iostream>
+#include <string>
 using namespace std;
 
 
 int main(){
-	//Write your code here.
+	int a =5;
+	char b = 'A';
+	char &c = b,*y = &b;
+	int *x = &a,*z = x;     
+		
+	cout << "a = " << a << "\t\t &a = " << &a <<"\n";
+	cout << "b = " << b << "\t\t &b = " << (int *)&b <<"\n";
+	cout << "c = " << c << "\t\t &c = " << (int *)&c <<"\n";
+	cout << "x = " << x << "\t &x = " << &x <<"\n";
+	cout << "y = " << (int *)y << "\t &y = " << &y <<"\n";
+	cout << "z = " << z << "\t &z = " << &z <<"\n\n";
 	
+	c = 'F';
+	b = c;
+	 cout << "a = "<< a <<"\n";
+	 cout << "b = "<< b <<"\n";
+	 cout << "c = "<< c <<"\n";
+	 cout << "x = "<< x <<"\n";
+	 cout << "y = "<< (int *)y <<"\n";
+	 cout << "z = "<< z <<"\n\n";
+	
+	*y = 'W';
+	 b = *y;
+	 cout << "a = "<< a <<"\n";
+	 cout << "b = "<< b <<"\n";
+	 cout << "c = "<< c <<"\n";
+	 cout << "x = "<< x <<"\n";
+	 cout << "y = "<< (int *)y <<"\n";
+	 cout << "z = "<< z <<"\n\n";
+	
+	 *x = 6;
+	 a = *x;
+	 cout << "a = "<< a <<"\n";
+	 cout << "b = "<< b <<"\n";
+	 cout << "c = "<< c <<"\n";
+	 cout << "x = "<< x <<"\n";
+	 cout << "y = "<< (int *)y <<"\n";
+	 cout << "z = "<< z <<"\n\n"; 
+	 
+	 *z = 7;
+	 a = *z;
+	 cout << "a = "<< a <<"\n";
+	 cout << "b = "<< b <<"\n";
+	 cout << "c = "<< c <<"\n";
+	 cout << "x = "<< x <<"\n";
+	 cout << "y = "<< (int *)y <<"\n";
+	 cout << "z = "<< z <<"\n\n";
 	
 	
 	return 0;
